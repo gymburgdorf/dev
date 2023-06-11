d36968d06bb68fb3ea1ff808165bffbdc4b1799b
gymburgdorf <adrian.luethi@gymburgdorf.ch>
2023-06-11 22:47:01 +0200

    import from remote

--- src/importWebcomponentES6.ts
+++ src/importWebcomponentES6.ts
@@ -1,5 +1,5 @@
-import {DevComponent} from "./DevComponent"
+import {DevComponent} from "https://gymburgdorf.github.io/dev/DevComponent.js"
 
-//const devy = new DevComponent()
+const devy = new DevComponent()
 
 document?.querySelector(".app")?.insertAdjacentHTML("beforeend", `<dev-component>123</dev-component>`)
